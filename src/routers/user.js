8eef9faea9ce0dc5d6fc2264ea18be754af9da06
manuel <manuelnaranjo@outlook.com>
2022-09-01 10:41:54 -0500

    Separate route files

---
+++ src/routers/user.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const User = require('../models/user');
+const router = new express.Router();
+
+router.post('/users', async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.get('/users', async (req, res) => {
+  try {
+    const users = await User.find();
+    res.send(users);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.get('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.patch('/users/:id', async (req, res) => {
+  try {
+    const updates = Object.keys(req.body);
+    const allowedUpdates = ['name', 'email', 'password'];
+    const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+
+    if (!isValidOperation) {
+      return res.status(400).send();
+    }
+
+    const user = await User.findById(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    updates.forEach((element) => (user[element] = req.body[element]));
+
+    await user.save();
+
+    res.send(user);
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+router.delete('/users/:id', async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+
+    res.send({ Response: 'User id: ' + user.id + ' deleted.' });
+  } catch (e) {
+    res.status(500).send(e);
+  }
+});
+
+module.exports = router;
