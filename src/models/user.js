e3c95c88d557e48f3cbb9e2add9093a94c702332
Manuel Naranjo <manuelnaranjo@outlook.com>
2022-09-05 15:35:38 -0500

    File uploads

--- src/models/user.js
+++ src/models/user.js
@@ -40,6 +40,9 @@ const userSchema = new mongoose.Schema({
       },
     },
   ],
+  avatar: {
+    type: Buffer
+  }
 }, {
   timestamps: true
 })
@@ -56,6 +59,7 @@ userSchema.methods.toJSON = function () {
 
   delete userObject.password
   delete userObject.tokens
+  delete userObject.avatar
 
   return userObject
 }
