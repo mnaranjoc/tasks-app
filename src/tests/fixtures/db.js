3c61b4a3873c0dd5515840073c4e31abdda0a16f
Manuel Naranjo <manuelnaranjo@outlook.com>
2022-09-06 00:25:36 -0500

    [Test] Task endpoints

--- src/tests/fixtures/db.js
+++ src/tests/fixtures/db.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const User = require('../../models/user')
+const Task = require('../../models/task')
 const jwt = require('jsonwebtoken')
 
 const userOneId = new mongoose.Types.ObjectId()
@@ -15,13 +16,52 @@ const userOne = {
   ],
 }
 
+const userTwoId = new mongoose.Types.ObjectId()
+const userTwo = {
+  _id: userTwoId,
+  name: 'app admin',
+  email: 'app@admin.com',
+  password: 'f4x2zRbI',
+  tokens: [
+    {
+      token: jwt.sign({ _id: userTwoId }, 'mytestkey'),
+    },
+  ],
+}
+
+const taskOne = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'First task',
+  completed: false,
+  owner: userOne._id,
+}
+
+const taskTwo = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Second task',
+  completed: true,
+  owner: userOne._id,
+}
+
+const taskThree = {
+  _id: new mongoose.Types.ObjectId(),
+  description: 'Third task',
+  completed: true,
+  owner: userTwo._id,
+}
+
 const setupDatabase = async () => {
   await User.deleteMany()
+  await Task.deleteMany()
   await new User(userOne).save()
+  await new User(userTwo).save()
+  await new Task(taskOne).save()
+  await new Task(taskTwo).save()
+  await new Task(taskThree).save()
 }
 
 const closeConnection = async () => {
   mongoose.connection.close()
 }
 
-module.exports = { userOneId, userOne, setupDatabase, closeConnection }
+module.exports = { userOneId, userOne, userTwoId, userTwo, taskOne, taskTwo, taskThree, setupDatabase, closeConnection }
